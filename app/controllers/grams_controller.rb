1036e1725fd7c93d01bf52a57df1f70dfe24da49
jwmunn <contact@justinmunn.co>
2017-04-13 21:13:10 -0800

    Created grams#show action.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -8,6 +8,13 @@ class GramsController < ApplicationController
     @gram = Gram.new
   end
 
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
+  end
+
   def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
